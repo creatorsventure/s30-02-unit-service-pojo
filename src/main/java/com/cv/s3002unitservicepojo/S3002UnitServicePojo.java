c5c1952dd54857d9db78b3f785a87609822c55cd
ram <ram_r@live.com>
2025-04-23 14:21:37 +0400

    BASE

---
+++ src/main/java/com/cv/s3002unitservicepojo/S3002UnitServicePojo.java
@@ -0,0 +1,13 @@
+package com.cv.s3002unitservicepojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3002UnitServicePojo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3002UnitServicePojo.class, args);
+    }
+
+}
---
+++ src/test/java/com/cv/s3002unitservicepojo/S3002UnitServicePojoTests.java
@@ -0,0 +1,13 @@
+package com.cv.s3002unitservicepojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3002UnitServicePojoTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
