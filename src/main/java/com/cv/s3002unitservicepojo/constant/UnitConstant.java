b542f1f76280fb3772f15fa06d637f12209df6dd
ram <ram_r@live.com>
2025-05-04 14:02:22 +0400

    COMMIT 04-05-2025

--- src/main/java/com/cv/s3002unitservicepojo/constant/UnitConstant.java
+++ src/main/java/com/cv/s3002unitservicepojo/constant/UnitConstant.java
@@ -2,10 +2,10 @@ package com.cv.s3002unitservicepojo.constant;
 
 public class UnitConstant {
 
-    private final static String APP_NAVIGATION_API_BIN = "/bin";
-    private final static String APP_NAVIGATION_API_DEVICE = "/device";
-    private final static String APP_NAVIGATION_API_MERCHANT = "/merchant";
-    private final static String APP_NAVIGATION_API_TRANSACTION_SUMMARY = "/transaction-summary";
-    private final static String APP_NAVIGATION_API_UNIT_OPTIONS = "/unit-options";
-    private final static String APP_NAVIGATION_API_UNIT_KEY = "/unit-key";
+    public final static String APP_NAVIGATION_API_BIN = "/bin";
+    public final static String APP_NAVIGATION_API_DEVICE = "/device";
+    public final static String APP_NAVIGATION_API_MERCHANT = "/merchant";
+    public final static String APP_NAVIGATION_API_TRANSACTION_SUMMARY = "/transaction-summary";
+    public final static String APP_NAVIGATION_API_UNIT_OPTIONS = "/unit-options";
+    public final static String APP_NAVIGATION_API_UNIT_KEY = "/unit-key";
 }
