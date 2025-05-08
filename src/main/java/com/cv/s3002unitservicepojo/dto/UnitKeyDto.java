701fa6b2bea9db944d982ef1dfc95625a996bf58
ram <ram_r@live.com>
2025-05-08 19:44:44 +0400

    COMMIT 08-05-2025

--- src/main/java/com/cv/s3002unitservicepojo/dto/UnitKeyDto.java
+++ src/main/java/com/cv/s3002unitservicepojo/dto/UnitKeyDto.java
@@ -20,16 +20,42 @@ public class UnitKeyDto extends GenericDto {
     @Serial
     private static final long serialVersionUID = 3921770346442372330L;
 
-    @NotBlank(message = "{app.message.failure.blank}")
-    @NotNull(message = "{app.message.failure.blank}")
-    private String password;
 
     @NotBlank(message = "{app.message.failure.blank}")
     @NotNull(message = "{app.message.failure.blank}")
-    String privateKey;
+    private String commonName;
 
     @NotBlank(message = "{app.message.failure.blank}")
     @NotNull(message = "{app.message.failure.blank}")
-    String publicKey;
+    private String organization;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String organizationalUnit;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String locality;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String state;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String country;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String email;
+
+    @NotNull(message = "{app.message.failure.blank}")
+    private Integer validityYears;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    private String unitPrivateKeyPassword;
+    private String unitPrivateKey;
+    private String unitCertificate;
 
 }
--- src/main/java/com/cv/s3002unitservicepojo/entity/UnitKey.java
+++ src/main/java/com/cv/s3002unitservicepojo/entity/UnitKey.java
@@ -22,14 +22,38 @@ public class UnitKey extends GenericUnitEntity {
     @Serial
     private static final long serialVersionUID = 4074762267809674292L;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String password;
+    @Column(nullable = false)
+    private String commonName;
+
+    @Column(nullable = false)
+    private String organization;
+
+    @Column(nullable = false)
+    private String organizationalUnit;
+
+    @Column(nullable = false)
+    private String locality;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer validityYears;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    String privateKey;
+    private String unitPrivateKeyPassword;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    String publicKey;
+    private String unitCertificate;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String unitPrivateKey;
 
     @OneToMany(mappedBy = "unitKey")
     @ToString.Exclude
